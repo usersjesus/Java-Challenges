27a7f619234cadb1a82601059086880a944f9483
sjesus <s.jesus10@yahoo.com>
2024-05-09 20:58:22 -0500

    Interest challenge

--- src/PrimeNumberChallenge.java
+++ src/PrimeNumberChallenge.java
@@ -2,5 +2,38 @@ public class PrimeNumberChallenge {
 
     public static void main(String[] args) {
 
+        System.out.println("The total number of prime number is " + primeCounter(6));
+
+    }
+
+    public static boolean isPrime(int wholeNumber) {
+
+        if (wholeNumber <= 2) {
+            return (wholeNumber == 2);
+        }
+
+        for (int i = 2; i <= wholeNumber / 2; i++) {
+            if (wholeNumber % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int primeCounter(int range) {
+
+        int count = 0;
+
+        for (int i = 2; i <= range; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+                count++;
+            }
+            if(count == 3) {
+                break;
+            }
+        }
+
+        return count;
     }
 }

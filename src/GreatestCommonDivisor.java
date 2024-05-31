fc8351a45476e31ba04454bbea57f0e156ea17e0
sjesus <s.jesus10@yahoo.com>
2024-05-30 20:56:39 -0500

    Refactored GreatestCommonDivisor

--- src/GreatestCommonDivisor.java
+++ src/GreatestCommonDivisor.java
@@ -1,10 +1,10 @@
 public class GreatestCommonDivisor {
     public static void main(String[] args) {
 
-        System.out.println(getGreatestCommonDivisor(25,15));
-        System.out.println(getGreatestCommonDivisor(12,30));
-        System.out.println(getGreatestCommonDivisor(9,18));
-        System.out.println(getGreatestCommonDivisor(81,153));
+        System.out.println(getGreatestCommonDivisor(25, 15));
+        System.out.println(getGreatestCommonDivisor(12, 30));
+        System.out.println(getGreatestCommonDivisor(9, 18));
+        System.out.println(getGreatestCommonDivisor(81, 153));
     }
 
     public static int getGreatestCommonDivisor(int first, int second) {
@@ -15,25 +15,15 @@ public class GreatestCommonDivisor {
 
         int curr = 1;
         int gcd = 0;
+        int min = Math.min(first, second);
 
-        if (first < second) {
-            while (curr <= first) {
-                if (first % curr == 0 && second % curr == 0) {
-                    gcd = curr;
-                }
 
-                curr++;
+        while (curr <= min) {
+            if (first % curr == 0 && second % curr == 0) {
+                gcd = curr;
             }
-        }
 
-        if (second < first) {
-            while (curr <= second) {
-                if (first % curr == 0 && second % curr == 0) {
-                    gcd = curr;
-                }
-
-                curr++;
-            }
+            curr++;
         }
 
         return gcd;

529dbaea1df197d3cbebc037d0727a0239fa1595
sjesus <s.jesus10@yahoo.com>
2024-06-02 12:18:22 -0500

    Refactored code in NumberToWords

--- src/NumberToWords.java
+++ src/NumberToWords.java
@@ -11,59 +11,14 @@ public class NumberToWords {
 
     }
 
-    public static void numberToWords(int number) {
-
-        if (number < 0) {
-            System.out.println("Invalid Value");
-        }
-
-        int reversedNumber = reverse(number);
-        int originalReversedNumber = reverse(number);
-        int numberOfZeros = getDigitCount(number) - getDigitCount(reversedNumber);
-
-        if (number == 0) {
-            System.out.println("Zero");
-            return;
-        }
-//        int numberOfDigits = getDigitCount(reversedNumber);
-
-        while (reversedNumber != 0) {
-            String digitInWords = switch (reversedNumber % 10) {
-                case 0 -> "Zero";
-                case 1 -> "One";
-                case 2 -> "Two";
-                case 3 -> "Three";
-                case 4 -> "Four";
-                case 5 -> "Five";
-                case 6 -> "Six";
-                case 7 -> "Seven";
-                case 8 -> "Eight";
-                case 9 -> "Nine";
-                default -> throw new IllegalStateException("Unexpected value: " + reversedNumber % 10);
-            };
-
-            System.out.println(digitInWords);
-            reversedNumber /= 10;
-
-                for(int i = 0; i < numberOfZeros; i++) {
-                    System.out.println("Zero");
-                }
-
-//            numberOfDigits = getDigitCount(reversedNumber /= 10);
-
-        }
-    }
-
     public static int reverse(int number) {
 
-        int reversedNumber = 0;
-
+        int reverse = 0;
         while (number != 0) {
-            reversedNumber = reversedNumber * 10 + (number % 10);
+            reverse = reverse * 10 + number % 10;
             number /= 10;
         }
-
-        return reversedNumber;
+        return reverse;
     }
 
     public static int getDigitCount(int number) {
@@ -72,16 +27,55 @@ public class NumberToWords {
             return -1;
         }
 
-        int count = 0;
-
         if (number == 0) {
             return 1;
         }
 
+        int count = 0;
+
         for (int i = 1; i <= number; i *= 10) {
             count++;
         }
-
         return count;
+    }
+
+    public static void numberToWords(int number) {
+
+        if (number < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        int reverse = reverse(number);
+
+        int leadingZeroes = getDigitCount(number) - getDigitCount(reverse);
+
+        if (number == 0) {
+            System.out.println("Zero");
+            return;
+        }
+
+
+        while (reverse != 0) {
+            int lastDigit = reverse % 10;
+            switch (lastDigit) {
+                case 0 -> System.out.println("Zero");
+                case 1 -> System.out.println("One");
+                case 2 -> System.out.println("Two");
+                case 3 -> System.out.println("Three");
+                case 4 -> System.out.println("Four");
+                case 5 -> System.out.println("Five");
+                case 6 -> System.out.println("Six");
+                case 7 -> System.out.println("Seven");
+                case 8 -> System.out.println("Eight");
+                case 9 -> System.out.println("Nine");
+            }
+            reverse /= 10;
+        }
+
+
+        for (int i = 0; i < leadingZeroes; i++) {
+            System.out.println("Zero");
+        }
     }
 }
